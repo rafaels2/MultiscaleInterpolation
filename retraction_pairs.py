351899aff5b8ea9890bca0cef061861e885d4f93
Rafael Sherbu Cohen <rafaels2@mail.tau.ac.il>
2020-03-31 19:28:19 +0300

    Added SPD exp-log

--- retraction_pairs.py
+++ retraction_pairs.py
@@ -2,10 +2,14 @@ import numpy as np
 from numpy import linalg as la
 from abc import abstractmethod
 
+from scipy.linalg import expm, logm, sqrtm
+from sklearn.datasets import make_spd_matrix
+
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 
 ALLOWED_AVERAGING_ERROR = 0.05
+SYMMETRIC_ERROR = 10 ** -5
 
 
 class AbstractManifold(object):
@@ -17,15 +21,27 @@ class AbstractManifold(object):
     def log(self, x, y):
         pass
 
+    def distance(self, x, y):
+        return la.norm(self.log(x, y))
+
     def calculate_error(self, x, y):
         error = np.zeros_like(x, dtype=np.float32)
         for index in np.ndindex(x.shape):
-            error[index] = la.norm(self.log(x[index], y[index]))
+            error[index] = self.distance(x[index], y[index])
         return error
 
     @abstractmethod
+    def _to_numbers(self, x):
+        pass
+
     def _visualize(self, plt, data):
-        pass    
+        visualization = np.zeros_like(data, dtype=np.float32)
+        for index in np.ndindex(visualization.shape):
+            x = data[index]
+            visualization[index] = self._to_numbers(x)
+        plt.imshow(visualization)
+        cb = plt.colorbar()
+        return cb    
 
     def plot(self, data, title, filename):
         plt.figure()
@@ -117,10 +133,8 @@ class PositiveNumbers(AbstractManifold):
             epsilon = 0
         return np.log(y) / (np.log(x) + epsilon)
 
-    def _visualize(self, plt, data):
-        plt.imshow(data)
-        cb = plt.colorbar()
-        return cb
+    def _to_numbers(self, x):
+        return x
 
     def zero_func(self, x_0, x_1):
         return 2
@@ -140,19 +154,14 @@ class Circle(AbstractManifold):
             inner_product = 0.00001
         return ((y / np.abs(inner_product)) - x)
 
-    def _visualize(self, plt, data):
+    def _to_numbers(self, x):
         """
         WARNING! this usage of arctan can be missleading - it can choose the 
         incorrect brach.
         I guess that plotting the log can be better.
         """
-        visualization = np.zeros_like(data, dtype=np.float32)
-        for index in np.ndindex(visualization.shape):
-            x = data[index]
-            visualization[index] = np.arctan2(x[1], x[0])
-        plt.imshow(visualization)
-        cb = plt.colorbar()
-        return cb
+        return np.arctan2(x[1], x[0])
+        
 
     def gen_point(self, phi):
         return np.array([np.cos(phi), np.sin(phi)])
@@ -179,14 +188,74 @@ class Circle(AbstractManifold):
         return self._geodetic_average(values_to_average, weights)
 
 
+class SymmetricPositiveDefinite(AbstractManifold):
+    def __init__(self, dim=3):
+        super().__init__()
+        self.dim = dim
+
+    def is_in_manifold(self, x):
+        return all([
+            (la.norm(x - np.transpose(x)) < SYMMETRIC_ERROR),
+            (all(x > 0 for x in la.eig(x)[0]))
+        ])
+
+    def exp(self, x, y):
+        sqrt_x = sqrtm(x)
+        exp_param = self._calculate_log_param(x, y)
+        return np.matmul(np.matmul(sqrt_x, expm(exp_param)), sqrt_x)
+
+    def old_log(self, x, y):
+        sqrt_x = sqrtm(x)
+        inv_sqrt_x = la.inv(sqrt_x) 
+        return sqrt_x * logm(inv_sqrt_x * y * inv_sqrt_x) * sqrt_x
+
+    def _calculate_log_param(self, x, y):
+        sqrt_x = sqrtm(x)
+        inv_sqrt_x = la.inv(sqrt_x) 
+        return np.matmul(np.matmul(inv_sqrt_x, y), inv_sqrt_x)
+        
+    def log(self, x, y):
+        sqrt_x = sqrtm(x)
+        log_param = self._calculate_log_param(x, y)
+        return np.matmul(np.matmul(sqrt_x, logm(log_param)), sqrt_x)
+
+    def distance(self, x, y):
+        log_param = self._calculate_log_param(x, y)
+        return la.norm(logm(log_param))
+
+    def _to_numbers(self, x):
+        return np.norm(x, ord=2)
+
+    def gen_point(self):
+        return make_spd_matrix(self.dim)
+
+    def zero_func(self, x_0, x_1):
+        return np.eye(self.dim)
+
+    def _get_geodetic_line(self, x, y):
+        sqrt_x = sqrtm(x)
+        log_param = self._calculate_log_param(x, y)
+
+        def line(t):
+            return sqrt_x * logm(t * log_param) * sqrt_x
+
+        return line
+
+    def _karcher_mean(self, values_to_average, weights):
+        raise NotImplementedError("TODO")
+
+    def average(self, values_to_average, weights):
+        return self._karcher_mean(values_to_average, weights)
+
+
 def main():
-    m = Circle()
-    a = m.gen_point(0)
-    b = m.gen_point(2)
-    e = m.gen_point(1)
-    f = m.gen_point (0.5)
-    s = m.average([a, b], [1, 1])
-    print("s: ", np.arctan2(s[1], s[0]))
+    m = SymmetricPositiveDefinite()
+    a = m.gen_point()
+    b = m.gen_point()
+    e = m.gen_point()
+    f = m.gen_point ()
+    # s = m.average([a, b], [1, 1])
+    # print("s: ", np.arctan2(s[1], s[0]))
     c = m.log(a, b)
     g = m.log(a, e)
     h = m.log(a, f)
@@ -195,8 +264,11 @@ def main():
     i = m.exp(a, c + g + h)
     j = m.exp(d, g + h)
 
-    print("d-b: ", d-b)
-    print("a^(c+g+h) - (a^c)^(g+h)", i-j)
+    for x in [d, i, j]:
+        print("is in", m.is_in_manifold(x))
+
+    print("d-b: ", m.distance(d, b))
+    print("a^(c+g+h) - (a^c)^(g+h)", m.distance(i, j))
 
 
 if __name__ == "__main__":
