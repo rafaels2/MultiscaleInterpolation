0f0b5f8f60feef0289d786199ee1c1b6e062388f
Rafael Sherbu Cohen <rafaels2@mail.tau.ac.il>
2020-04-11 19:22:22 +0300

    Added SO(3)

--- Config.py
+++ Config.py
@@ -8,6 +8,7 @@ _SCALING_FACTOR = 0.7
 
 
 def _original_function(x, y):
+    # TODO: add check if function returns a valid manifold point.
     z = (np.abs(np.cos(7*y)) + 0.1) * np.exp(-x**2 -y**2) * (5 * np.eye(3) + np.array([[np.sin(5 * y), y, x*y], [0, 0, y ** 2],[0,0,0]]))
     return (z + np.transpose(z))
 
---
+++ Manifolds/RigidRotations.py
@@ -0,0 +1,121 @@
+import scipy
+import numpy as np
+from numpy import linalg as la
+
+from pyquaternion import Quaternion
+from scipy.stats import special_ortho_group
+from scipy.spatial.transform import Rotation
+
+from Tools.KarcherMean import KarcherMean
+
+from .AbstractManifold import AbstractManifold
+
+SPECIAL_TOLERANCE = 0.001
+ORTHOGONAL_TOLERANCE = 0.001
+
+
+class RigidRotations(AbstractManifold):
+    def __init__(self, dim=3):
+        super().__init__()
+        self.dim = dim
+
+    def zero_func(self, x_0, x_1):
+        return np.eye(self.dim)
+
+    def distance(self, x, y):
+        x_quat = self._quaternion_from_matrix(x)
+        y_quat = self._quaternion_from_matrix(y)
+        return Quaternion.distance(x_quat, y_quat)
+
+    def _quaternion_from_matrix(self, matrix):
+        """
+        :param matrix: Rotation matrix.
+        :return: Quaternion representation of the given matrix.
+        """
+        return Quaternion(Rotation.from_matrix(matrix).as_quat())
+
+    def _matrix_from_quaternion(self, quaternion):
+        """
+        :param quaternion: Quaternion representation of a given rotation matrix.
+        :return: The rotation matrix.
+        """
+        return Rotation.from_quat(quaternion.elements).as_matrix()
+
+    def log(self, x, y):
+        x_quat = self._quaternion_from_matrix(x)
+        y_quat = self._quaternion_from_matrix(y)
+
+        return self._matrix_from_quaternion(Quaternion.log_map(x_quat, y_quat))
+
+    def exp(self, x, y):
+        x_quat = self._quaternion_from_matrix(x)
+        y_quat = self._quaternion_from_matrix(y)
+
+        return self._matrix_from_quaternion(Quaternion.exp_map(x_quat, y_quat))
+
+    def gen_point(self):
+        matrix = np.array(special_ortho_group.rvs(self.dim))
+        return matrix
+
+    def zero_func(self, x_0, x_1):
+        return np.eye(self.dim)
+
+    def is_in_manifold(self, matrix):
+        """
+        Is in SO(3) predicate.
+        :param matrix: Examined matrix.
+        :return: bool
+        """
+        is_orthogonal = (la.norm(np.matmul(matrix, np.transpose(matrix)) - np.eye(self.dim)) < ORTHOGONAL_TOLERANCE)
+        is_special = np.abs(la.det(matrix) - 1) < SPECIAL_TOLERANCE
+        answer = is_special and is_orthogonal
+        if not answer:
+            print(matrix, is_special, is_orthogonal)
+        return answer
+
+    def geodesic_l2_mean_step(self, current_estimator, noisy_samples, weights, tolerance=0.0001):
+        projected_diff_samples = [w_i * scipy.linalg.logm(np.matmul(np.linalg.inv(current_estimator), noisy_sample)) 
+                                  for w_i, noisy_sample in
+                                  zip(weights, noisy_samples)]
+        matrices_sum = np.zeros(projected_diff_samples[0].shape, dtype='complex128')
+        for projected_diff_matrix in projected_diff_samples:
+            matrices_sum += projected_diff_matrix
+        r = matrices_sum / sum(weights)
+        projected_avg_norm = np.linalg.norm(r, ord=2)
+        if projected_avg_norm < tolerance:
+            state_of_convergence = True
+            return current_estimator, state_of_convergence
+        else:
+            new_estimator = np.matmul(current_estimator, scipy.linalg.expm(r))
+            state_of_convergence = False
+            return new_estimator, state_of_convergence
+
+    def geodesic_l2_mean(self, noisy_samples, weights, tolerance=0.001, maximum_iteration=10):
+        mean_estimator = noisy_samples[0]
+        mean_estimator_list = [mean_estimator]
+        state_of_convergence = False
+        run_index = 0
+        while not state_of_convergence and run_index < maximum_iteration:
+            mean_estimator, state_of_convergence = self.geodesic_l2_mean_step(
+                mean_estimator, noisy_samples, weights, tolerance)
+            mean_estimator_list.append(mean_estimator)
+            run_index += 1
+        return mean_estimator_list
+
+    def average(self, values_to_average, weights, base=np.eye(3)):
+        return self.geodesic_l2_mean(values_to_average, weights)[-1]
+
+
+def main():
+    m = RigidRotations()
+
+    a = m.gen_point()
+    b = m.gen_point()
+    c = m.gen_point()
+
+    d = m.average([a,b,c], [1,1,1])
+    return a,b,c,d,m
+
+
+if __name__ == '__main__':
+    a,b,c,d,m = main()
