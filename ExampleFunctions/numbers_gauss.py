7944dce8a1ef183677b2b618a462f513fdfcb8c6
Rafael S. Cohen <rafaels2@mail.tau.ac.il>
2020-08-08 19:03:43 +0300

    good results on high frequencies.

--- ExampleFunctions/numbers_gauss.py
+++ ExampleFunctions/numbers_gauss.py
@@ -2,5 +2,7 @@ import numpy as np
 
 
 def original_function(x, y):
-    return 5 * (np.exp(-x**2 - y**2))
+    # return 5 * (np.exp(-x**2 - y**2))
+    # return np.sin(x - y)
+    return np.sin(4*x)*np.cos(5*y) - x ** 2 - y ** 2
 
