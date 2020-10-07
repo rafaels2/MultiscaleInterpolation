3e0ec805a86c3d434355978cb42b4e07b3b682c9
Rafael S. Cohen <rafaels2@mail.tau.ac.il>
2020-10-07 11:58:34 +0300

    Code Cleanup in Quasi.py

--- ApproximationMethods/Quasi.py
+++ ApproximationMethods/Quasi.py
@@ -1,20 +1,20 @@
 import numpy as np
 from cachetools import cached
-from numpy import linalg as la
-from collections import namedtuple
-from Tools.Utils import generate_grid, generate_kernel, evaluate_on_grid, generate_cache
 
-from .ApproximationMethod import ApproximationMethod
 from Tools.SamplingPoints import SamplingPointsCollection
+from Tools.Utils import generate_kernel, generate_cache
+from .ApproximationMethod import ApproximationMethod
+
 
 def combine(a, b):
     def func(x, y):
         return a(x, y), b(x, y)
+
     return func
 
 
 class Quasi(ApproximationMethod):
-    def __init__(self, manifold, original_function, grid_parameters, rbf, 
+    def __init__(self, manifold, original_function, grid_parameters, rbf,
                  scale, is_approximating_on_tangent):
         if isinstance(original_function, tuple):
             original_function = combine(*original_function)
@@ -24,11 +24,11 @@ class Quasi(ApproximationMethod):
         super().__init__(manifold, original_function, grid_parameters, rbf)
         self._is_approximating_on_tangent = is_approximating_on_tangent
         rbf_radius = scale
-        
-        self._grid = SamplingPointsCollection(rbf_radius, 
-            original_function,
-            grid_parameters,
-            phi_generator=self._calculate_phi)
+
+        self._grid = SamplingPointsCollection(rbf_radius,
+                                              original_function,
+                                              grid_parameters,
+                                              phi_generator=self._calculate_phi)
 
         self._kernel = generate_kernel(self._rbf, rbf_radius)
 
@@ -41,12 +41,13 @@ class Quasi(ApproximationMethod):
             return self._kernel(vector, point)
 
         return phi
-    
+
     @cached(cache=generate_cache(maxsize=1000))
     def approximation(self, x, y):
         """ Average sampled points around (x, y), using phis as weights """
         values_to_average = list()
         weights = list()
+        base = None
 
         if self._is_adaptive:
             base = self._original_function(x, y)[1]
@@ -69,5 +70,5 @@ class Quasi(ApproximationMethod):
 
         if self._is_adaptive:
             return self._manifold.log(base, self._manifold.average(values_to_average, weights))
-        
+
         return self._manifold.average(values_to_average, weights)
