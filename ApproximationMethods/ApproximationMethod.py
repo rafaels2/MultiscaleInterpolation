5238c8a1cacf44bb082e421bb5e1010dd859fd93
Rafael S. Cohen <rafaels2@mail.tau.ac.il>
2021-02-05 13:11:22 +0200

    fixed Tabs in ApproximationMethod.py

--- ApproximationMethods/ApproximationMethod.py
+++ ApproximationMethods/ApproximationMethod.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+
 import numpy as np
 from cachetools import cached
 
@@ -6,25 +7,26 @@ from Tools.Utils import generate_cache
 
 
 class ApproximationMethod(object):
-	def __init__(self, manifold, original_function, grid_parameters, rbf):
-		self._original_function = original_function
-		self._grid_parameters = grid_parameters
-		self._rbf = rbf
+    def __init__(self, manifold, original_function, grid_parameters, rbf):
+        self._original_function = original_function
+        self._grid_parameters = grid_parameters
+        self._rbf = rbf
+
         self._manifold = manifold
-		# TODO: where do we get the kernel?
+        # TODO: where do we get the kernel?
         self._kernel = None
 
-	@abstractmethod
-	def approximation(self, x, y):
-		# TODO: change (x, y) to p.
-		pass
+    @abstractmethod
+    def approximation(self, x, y):
+        # TODO: change (x, y) to p.
+        pass
 
-	def _calculate_phi(self, x_0, y_0):
-		point = np.array([x_0, y_0])
+    def _calculate_phi(self, x_0, y_0):
+        point = np.array([x_0, y_0])
 
-		@cached(cache=generate_cache(maxsize=100))
-		def phi(x, y):
-			vector = np.array([x, y])
-			return self._kernel(vector, point)
+        @cached(cache=generate_cache(maxsize=100))
+        def phi(x, y):
+            vector = np.array([x, y])
+            return self._kernel(vector, point)
 
-		return phi
+        return phi
